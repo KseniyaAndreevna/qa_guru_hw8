e55d948588c447ad9f041c391d138ae21caf38d0
ksu <bindyurova.kseniya@gmail.com>
2022-01-31 20:16:09 +0700

    Refactor isEvenNumber() method and add two tests

---
+++ src/test/java/guru/qa/Tests.java
@@ -0,0 +1,19 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Tests {
+
+    @Test
+    void test1() {
+        Assertions.assertTrue(100 <= 100);
+    }
+
+    @Test
+    void test2() {
+        String string1 = new String("JAVA");
+        String string2 = "Java";
+        Assertions.assertTrue(string1.equalsIgnoreCase(string2));
+    }
+}
